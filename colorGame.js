d1e8d3c4a6a6004db773097f2c13b035d9b5c0d2
DAI ANDO <daifloss@gmail.com>
2017-08-06 13:44:40 +0900

    part2

--- colorGame.js
+++ colorGame.js
@@ -8,21 +8,35 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
   squares[i].style.background = colors[i];
 
-  squares[i].addEventListner("click", function(){
-    clickedColor = this.style.background;
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.background;
 
     if(clickedColor === pickedColor){
-
+      messageDisplay.textContent = "Correct !";
+      changeColors(clickedColor);
     } else {
-
+      this.style.background = "#232323"
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color) {
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.background =  color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
