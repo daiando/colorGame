56436d8e5c63bb11478a28611aa6ec74c417c423
Dai Ando <dai.ando@rakuten.com>
2017-09-12 17:41:00 +0900

    add

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,56 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector('#easyBtn');
+var hardBtn = document.querySelector('#hardBtn');
 
+
+easyBtn.addEventListener('click', function() {
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+    if(colors[i]) {
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = 'none';
+    }
+  }
+})
+hardBtn.addEventListener('click', function() {
+  easyBtn.classList.remove('selected');
+  hardBtn.classList.add('selected');
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+    squares[i].style.display = 'block';
+  }
+})
+
+resetButton.addEventListener('click', function(){
+  colors = generateRandomColors(numSquares);
+  //  pick a color
+  pickedColor = pickColor();
+  // change
+  colorDisplay.textContent = pickedColor;
+
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+  }
+
+  h1.style.background = 'steelblue';
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,10 +62,11 @@ for(var i = 0; i < squares.length; i++) {
 
     if(clickedColor === pickedColor){
       messageDisplay.textContent = "Correct !";
+      resetButton.textContent = 'Play Aain ?'
       changeColors(clickedColor);
       h1.style.background = clickedColor;
     } else {
-      this.style.background = "#232323"
+      this.style.background = "steelblue"
       messageDisplay.textContent = "Try Again";
     }
   });
