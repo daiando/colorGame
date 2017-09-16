0334bb62248ed614278669dd4638bdd30c37c548
Dai Ando <dai.ando@rakuten.com>
2017-09-16 09:26:29 +0900

    wrap up

--- colorGame.js
+++ colorGame.js
@@ -1,42 +1,82 @@
+// var game = {};
+// game.init = function(){
+//   setupModeButtons();
+//   setupSquares();
+//   reset();
+// }
+//
+// game.init();
+
 var numSquares = 6;
-var colors = generateRandomColors(numSquares);
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector('#easyBtn');
-var hardBtn = document.querySelector('#hardBtn');
+var modeButtons = document.querySelectorAll(".mode");
 
+init();
 
-easyBtn.addEventListener('click', function() {
-  hardBtn.classList.remove('selected');
-  easyBtn.classList.add('selected');
-  numSquares = 3;
+function init() {
+  setupModeButtons();
+  setupSquares();
+  reset();
+}
+
+function setupModeButtons() {
+  for(var i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function() {
+      modeButtons[0].classList.remove("selected");
+      modeButtons[1].classList.remove("selected");
+      this.classList.add("selected");
+      this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+
+      reset();
+    });
+  }
+}
+
+function setupSquares() {
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", function(){
+      var clickedColor = this.style.background;
+
+      if(clickedColor === pickedColor){
+        messageDisplay.textContent = "Correct !";
+        resetButton.textContent = 'Play Again ?'
+        changeColors(clickedColor);
+        h1.style.background = clickedColor;
+      } else {
+        this.style.background = "#232323"
+        messageDisplay.textContent = "Try Again";
+      }
+    });
+  }
+}
+
+function reset() {
   colors = generateRandomColors(numSquares);
+  //  pick a color
   pickedColor = pickColor();
+  // change
   colorDisplay.textContent = pickedColor;
+  resetButton.textContent = 'NEW COLORS'
+  messageDisplay.textContent = '';
+
   for(var i = 0; i < squares.length; i++) {
     if(colors[i]) {
+      squares[i].style.display = 'block';
       squares[i].style.background = colors[i];
     } else {
       squares[i].style.display = 'none';
     }
   }
-})
-hardBtn.addEventListener('click', function() {
-  easyBtn.classList.remove('selected');
-  hardBtn.classList.add('selected');
-  numSquares = 6;
-  colors = generateRandomColors(numSquares);
-  pickedColor = pickColor();
-  colorDisplay.textContent = pickedColor;
-  for(var i = 0; i < squares.length; i++) {
-    squares[i].style.background = colors[i];
-    squares[i].style.display = 'block';
-  }
-})
+
+  h1.style.background = 'steelblue';
+}
+
 
 resetButton.addEventListener('click', function(){
   colors = generateRandomColors(numSquares);
@@ -44,6 +84,8 @@ resetButton.addEventListener('click', function(){
   pickedColor = pickColor();
   // change
   colorDisplay.textContent = pickedColor;
+  resetButton.textContent = 'NEW COLORS'
+  messageDisplay.textContent = '';
 
   for(var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors[i];
@@ -54,23 +96,6 @@ resetButton.addEventListener('click', function(){
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++) {
-  squares[i].style.background = colors[i];
-
-  squares[i].addEventListener("click", function(){
-    var clickedColor = this.style.background;
-
-    if(clickedColor === pickedColor){
-      messageDisplay.textContent = "Correct !";
-      resetButton.textContent = 'Play Aain ?'
-      changeColors(clickedColor);
-      h1.style.background = clickedColor;
-    } else {
-      this.style.background = "steelblue"
-      messageDisplay.textContent = "Try Again";
-    }
-  });
-}
 
 function changeColors(color) {
   for(var i = 0; i < squares.length; i++){
